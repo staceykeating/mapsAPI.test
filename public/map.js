8bffe2e3c82a4820271135ec85fa8ce86407f829
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-04 02:16:24 +0000

    2nd commit

--- public/map.js
+++ public/map.js
@@ -1,14 +1,20 @@
 
-  let map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 49.2827, lng: -123.1207},
-      zoom: 10,
-      disableDefaultUI: true
-    });
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 49.2827, lng: -123.1207},
-      zoom: 10,
-      disableDefaultUI: true
-    });
-  }
+
+
+let map;
+let map1;
+
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.2827, lng: -123.1207},
+    zoom: 8,
+    disableDefaultUI: true
+  });  
+
+  map1 = new google.maps.Map(document.getElementById('map1'), {
+    center: {lat: 49.2827, lng: -123.1207},
+    zoom: 10,
+    disableDefaultUI: true
+  }); 
+};
+initialize();
